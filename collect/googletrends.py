1b3e53c73a28783bf5351d5562c7d13ed946986b
M. Yetisir <myetisir@golder.com>
2018-10-15 21:16:33 -0700

    updates googel trends data collection to be more robust

--- collect/googletrends.py
+++ collect/googletrends.py
@@ -21,8 +21,8 @@ def load_data(coin_list, from_date, to_date):
     return pd.concat(google_trends_dfs, axis='columns')
 
 
-def load_google_trends(coin, from_date, to_date, sample_interval=4,
-                       overlap_interval=1, cat=7, sleeptime=60):
+def load_google_trends(coin, from_date, to_date, sample_interval=1,
+                       overlap_interval=1, cat=0, sleeptime=60):
 
     sample_interval = int(sample_interval)
     if sample_interval + overlap_interval > 7:
@@ -46,42 +46,50 @@ def load_google_trends(coin, from_date, to_date, sample_interval=4,
         interval_end = query_start + (i+1)*sample_interval
         time_format = '%Y-%m-%dT%H'
 
-        interval_start = interval_start.strftime(time_format)
-        interval_end = interval_end.strftime(time_format)
-        timeframe = interval_start + ' ' + interval_end
+        int_start_str = interval_start.strftime(time_format)
+        int_end_str = interval_end.strftime(time_format)
+        timeframe = int_start_str + ' ' + int_end_str
 
         cached_file_name = ('google-trends_{coin}_{from_date}_to_'
                             '{to_date}.pkl').format(coin=coin,
-                                                    from_date=interval_start,
-                                                    to_date=interval_end)
+                                                    from_date=int_start_str,
+                                                    to_date=int_end_str)
 
         cached_file_path = os.path.join(os.path.pardir, 'cache',
                                         cached_file_name)
         if os.path.isfile(cached_file_path):
             with open(cached_file_path, 'rb') as file:
+                print('LOADING FROM CACHE: ', coin, timeframe)
                 interval_df = pickle.load(file)
         else:
             try:
                 print('DOWNLOADING: ', coin, timeframe)
                 pytrend.build_payload([coin], cat=cat, timeframe=timeframe)
                 interval_df = pytrend.interest_over_time()
-
+                interval_df['datetime'] = pd.to_datetime(interval_df.index)
+                interval_df = interval_df.set_index('datetime')
                 with open(cached_file_path, 'wb') as file:
                     pickle.dump(interval_df, file)
-            except:
-                print('FAILED: ', coin, timeframe)
+            except Exception as error:
+                print('FAILED: ', coin, timeframe, error)
+
                 pass
 
-        # TODO: use overlap interval for slicing instead of hard coded.
-        if len(coin_df) > 24:
-            last_24_hrs = coin_df.iloc[-24:]
-            first_24_hrs = interval_df.iloc[:24]
-            last_average = last_24_hrs.mean()
-            first_average = first_24_hrs.mean()
-            interval_df.loc[:, coin] = (interval_df.loc[:, coin] *
-                                        last_average / first_average)
+        if len(coin_df) > 0:
+            if coin_df.index[-1] - coin_df.index[0] > overlap_interval:
+                previous_interval_overlap = coin_df.loc[(interval_start -
+                                                         overlap_interval):,
+                                                        coin]
+                current_interval_overlap = interval_df.loc[:(interval_start +
+                                                             overlap_interval),
+                                                           coin]
+                previous_average = previous_interval_overlap.mean()
+                current_average = current_interval_overlap.mean()
 
-        coin_df = coin_df.append(interval_df)
+                interval_df.loc[:, coin] = (interval_df[coin] *
+                                            previous_average / current_average)
+        coin_df = coin_df.append(interval_df.loc[(interval_start +
+                                                  overlap_interval):])
 
     return coin_df
 
@@ -90,17 +98,16 @@ if __name__ == '__main__':
                  'ETH': 'ethereum',
                  'XRP': 'ripple',
                  'BCH': 'bitcoin cash',
-                 'EOS': 'eos'}
-
-    """                 'XLM': 'stellar',
+                 'EOS': 'eos',
+                 'XLM': 'stellar',
                  'LTC': 'litecoin',
                  'ADA': 'cardano',
-                 'XMR': 'monero',
+                 # 'XMR': 'monero',
                  'TRX': 'tron',
                  'MIOTA': 'iota',
                  'DASH': 'dash',
                  'BNB': 'binance',
-                 'NEO': 'neo'}"""
+                 'NEO': 'neo'}
 
     start_date = datetime.datetime(year=2017, month=1, day=1, hour=0)
     end_date = datetime.datetime.today()
