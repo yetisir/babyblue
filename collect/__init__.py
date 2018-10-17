9073cc712b7c4b48aea50241155cfcb46da8042a
M. Yetisir <myetisir@golder.com>
2018-10-16 20:46:38 -0700

    updates base collector class and adds comments

--- collect/__init__.py
+++ collect/__init__.py
@@ -6,12 +6,13 @@ import time
 
 class DataCollector(object):
     def __init__(self, collector_name, keyword, start_date, end_date,
-                 sample_interval, overlap_interval=0,
+                 sample_interval, overlap_interval=0, wait=1,
                  time_format='%Y-%m-%dT%H'):
 
         self.collector_name = collector_name
         self.keyword = keyword
         self.time_format = time_format
+        self.wait = wait
 
         # convert dates and times to datetime objects
         self.start_date = datetime.datetime(start_date)
@@ -41,50 +42,54 @@ class DataCollector(object):
         self.keyword_df = pd.DataFrame()
 
     def query_data(self):
-        for i in range(num_intervals):
-            self.interval_start = (query_start + i*sample_interval -
-                                   overlap_interval)
-            self.interval_end = (query_start + (i+1)*sample_interval)
+        # loop through each interval and compile a dataset
+        for i in range(self.num_intervals):
+            # determing the bounding datetimes for the interval
+            self.interval_start = (self.query_start + i*self.sample_interval -
+                                   self.overlap_interval)
+            self.interval_end = (self.query_start + (i+1)*self.sample_interval)
 
+            # convert the interval bounds to strings
             self.interval_start_str = self.interval_start.strftime(
                 self.time_format)
             self.interval_end_str = self.interval_end.strftime(
                 self.time_format)
 
+            # check to see if the interval has already been downloaded
             complete_interval = True
-            if os.path.isfile(cached_file_path):
+            cache_file = self.cache_file_path()
+            if os.path.isfile(cache_file):
                 interval_df = self.load_cache()
                 if interval_df.index[-1] < self.interval_end:
                     complete_interval = False
 
-            if not complete_interval or not os.path.isfile(cached_file_path):
+            # download the interval again if it does not exist or is incomplete
+            if not complete_interval or not os.path.isfile(cache_file):
                 try:
+                    # TODO replace with more appropriate logging method
                     print('DOWNLOADING: ', self.keyword, self.interval_start,
                           self.interval_end)
+
+                    # query method is defined by the child class
                     self.query()
-                    self.dump_cache(start_date, end_date)
-                    time.sleep(wait)
+                    self.dump_cache()
+                    time.sleep(self.wait)
+
+                # catch the exception and pass to child class
                 except Exception as error:
+                    # TODO replace with more appropriate logging method
+                    print('FAILED: ', self.keyword, self.interval_start,
+                          self.interval_end)
+
+                    # handle_query_error method is defined by the child class
                     self.handle_query_error(error)
 
+            # merge data if specified and not the first interval
             if self.overlap_interval and len(self.keyword_df) > 0:
                 self.merge_overlap()
 
-            coin_df = coin_df.append(self.interval_df)
-
-    def query(self): #Class Specific
-        pytrend.build_payload([coin], cat=cat, timeframe=timeframe)
-        interval_df = pytrend.interest_over_time()
-        interval_df['datetime'] = pd.to_datetime(interval_df.index)
-        interval_df = interval_df.set_index('datetime')
-
-    def handle_query_error(self, error): #Class Sepcific
-        if int(str(error)[-4:-1]) == 429:
-            print('QUERY LIMIT REACHED: waiting {0} '
-                  'seconds'.format(sleeptime))
-            time.sleep(sleeptime)
-        else:
-            print('FAILED: ', coin, timeframe, error)
+            # concatenate the interval to the dataframe
+            self.keyword_df = self.keyword_df.append(self.interval_df)
 
     def cache_file_path(self):
         # compile cache file name
@@ -103,6 +108,7 @@ class DataCollector(object):
 
         # load serialized data from cache
         with open(cache_file, 'rb') as file:
+            # TODO replace with more appropriate logging method
             print('LOADING FROM CACHE: ', self.keyword, self.interval_start,
                   self.interval_end)
             data = pickle.load(file)
@@ -119,24 +125,35 @@ class DataCollector(object):
             pickle.dump(self.interval_df, file)
 
     def merge_overlap(self):
+
+        # get timedelta of existing data
         df_time_range = self.keyword_df.index[-1] - self.keyword_df.index[0]
 
+        # select all of overalp data if it exists
         if df_time_range > self.overlap_interval:
             overlap_start = self.interval_start
             overlap_end = self.interval_start + self.overlap_interval
+
+        # otherwise only select range of existing data to merge
         else:
             overlap_start = self.keyword_df.index[0]
             overlap_end = self.keyword_df.index[-1]
 
+        # get overlapping series
         previous_interval_overlap = self.keyword_df.loc[overlap_start:,
                                                         self.keyword]
         current_interval_overlap = self.interval_df.loc[:overlap_end,
                                                         self.keyword]
+
+        # get averages of overalpping series
         previous_average = previous_interval_overlap.mean()
         current_average = current_interval_overlap.mean()
 
+        # normalize new interval based on the overlap with the old interval
         self.interval_df.loc[:, self.keyword] = (
             self.interval_df[self.keyword] * (previous_average /
                                               current_average))
+
+        # chop off overlapping data from new interval
         self.interval_df = self.interval_df.loc[(self.interval_start +
                                                  self.overlap_interval):]
