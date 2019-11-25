81d51d0988453485088c4023ed58d95af8634014
M. Yetisir <myetisir@golder.com>
2019-11-24 20:14:04 -0800

    first attempt at reddit spider

--- crawl/spiders/reddit.py
+++ crawl/spiders/reddit.py
@@ -47,96 +47,93 @@ class RedditSpider(Spider):
         self.close_mongodb()
 
     def parse(self, response):
-        if response.meta.get('type') is None:
-            for board in self.boards_to_crawl:
-                board_url = self.start_pushshift_board_threads_url.format(
-                    subreddit=board)
-                board_item = self.parse_board(board)
-                yield response.follow(
-                    board_url,
-                    callback=self.parse,
-                    meta={
-                        'type': 'threads',
-                        'board': board_item,
-                        'add_board': True}
-                )
-        elif response.meta.get('type') == 'threads':
+        for board in self.boards_to_crawl:
+            board_url = self.start_pushshift_board_threads_url.format(
+                subreddit=board)
+            board_item = self.parse_board(board)
+            import pdb; pdb.set_trace()
+            yield response.follow(
+                board_url,
+                callback=self.threads,
+                meta={
+                    'board': board_item,
+                    'add_board': True}
+            )
 
-            board = response.meta.get('board')
-            threads = json.loads(response.text)['data']
-            min_timestamp = float('inf')
-            for i, thread in enumerate(threads):
-                item, timestamp = self.parse_thread(thread)
-                comments_url = self.pushshift_thread_comments.format(
-                    thread=item['id']
-                )
-                min_timestamp = min(timestamp, min_timestamp)
-                if i == 0 and response.meta['add_board'] is True:
-                    add_board = True
-                yield response.follow(
-                    comments_url,
-                    callback=self.parse,
-                    meta={
-                        'board': response.meta['board'],
-                        'thread': item,
-                        'type': 'comment_ids',
-                        'add_board': add_board,
-                    }
-                )
+    def threads(self, response):
+
+        board = response.meta.get('board')
+        threads = json.loads(response.text)['data']
+        min_timestamp = float('inf')
+        for i, thread in enumerate(threads):
+            item, timestamp = self.parse_thread(thread)
+            comments_url = self.pushshift_thread_comments.format(
+                thread=item['id']
+            )
+            min_timestamp = min(timestamp, min_timestamp)
+            if i == 0 and response.meta['add_board'] is True:
+                add_board = True
+            else:
+                add_board = False
+            yield response.follow(
+                comments_url,
+                callback=self.comment_ids,
+                meta={
+                    'board': response.meta['board'],
+                    'thread': item,
+                    'add_board': add_board,
+                }
+            )
 
             if len(threads) != 0:
                 board_url = self.pushshift_board_threads_url.format(
-                    subreddit=board, before_timestamp=timestamp)
+                    subreddit=board['name'], before_timestamp=timestamp)
                 board_item = self.parse_board(board)
                 yield response.follow(
                     board_url,
-                    callback=self.parse,
+                    callback=self.threads,
                     meta={
-                        'type': 'threads',
                         'board': board_item,
                         'add_board': False}
                 )
 
-        elif response.meta.get('type') == 'comment_ids':
+    def comment_ids(self, response):
+        comment_ids = json.loads(response.text)['data']
 
-            comment_ids = json.loads(response.text)['data']
+        for i in range(0, len(comment_ids), 500):
+            comment_string = ','.join(comment_ids[i:i+500])
+            comments_url = self.pushshift_comments_url.format(
+                comments=comment_string)
 
-            for i in range(0, len(comment_ids), 500):
-                comment_string = ','.join(comment_ids[i:i+500])
-                comments_url = self.pushshift_comments_url.format(
-                    comments=comment_string)
+            yield response.follow(
+                comments_url,
+                callback=self.comments,
+                meta={
+                    'board': response.meta['board'],
+                    'thread': response.meta['thread'],
+                    'index': i,
+                    'add_board': response.meta['add_board'],
+                }
+            )
 
-                yield response.follow(
-                    comments_url,
-                    callback=self.parse,
-                    meta={
-                        'board': response.meta['board'],
-                        'thread': response.meta['thread'],
-                        'type': 'comments',
-                        'index': i,
-                        'add_board': response.meta['add_board'],
-                    }
-                )
+    def comments(self, response):
+        comment_timestamps = []
+        comments = json.loads(response.text)['data']
+        for comment in comments:
+            item = self.parse_comment(comment, response.meta['thread']['id'])
+            comments.append(item['timestamp'])
+            yield item
 
-        elif response.meta.get('type') == 'comments':
+        if response.meta.get('index') == 0:
+            thread_item = response.meta.get('thread')
+            thread_item['last_post'] = max(comment_timestamps)
+            yield thread_item
 
-            comment_timestamps = []
-            comments = json.loads(response.text)['data']
-            for comment in comments:
-                item = self.parse_comment(comment, response.meta['thread']['id'])
-                comments.append(item['timestamp'])
-                yield item
-
-            if response.meta.get('index') == 0:
-                thread_item = response.meta.get('thread')
-                thread_item['last_post'] = max(comment_timestamps)
-                yield thread_item
-
-                if response.meta['add_board'] is True:
-                    board_item = response.meta('board')
-                    board_item['last'] = thread_item['last_post']
-                    board_item['id'] = thread_item['board']
-                    yield board_item
+            if response.meta['add_board'] is True:
+                board_item = response.meta('board')
+                board_item['last'] = thread_item['last_post']
+                board_item['id'] = thread_item['board']
+                yield board_item
 
     def parse_thread(self, thread):
         item = Thread()
@@ -159,7 +156,6 @@ class RedditSpider(Spider):
     def parse_comment(self, comment, thread_id):
 
         item = Comment()
-        import pdb; pdb.set_trace()
         item['author'] = comment['author']
         item['text'] = comment['body']
         item['timestamp'] = datetime.utcfromtimestamp(comment['created_utc'])
